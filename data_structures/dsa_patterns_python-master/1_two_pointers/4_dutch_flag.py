45d5a7585b7bd3cc8f9fb45e1c9d4d50d4a45c7d
rashmik-01 <rashmi.rajendra.kulkarni@gmail.com>
2026-09-25 19:42:01 -0700

    [4_dutch_flag] - rename arr to nums

--- data_structures/dsa_patterns_python-master/1_two_pointers/4_dutch_flag.py
+++ data_structures/dsa_patterns_python-master/1_two_pointers/4_dutch_flag.py
@@ -1,15 +1,15 @@
 # O(N) time | O(1) space
-def dutch_flag_sort(arr):
+def dutch_flag_sort(nums):
     # Indexes point to where next 0 and 2 element should be put
     # all elements < next_zero should be 0, all elements > next_two should be 2
     # once we put 0 and 2 in their right places, 1 will automatically be in its right place
-    next_zero, next_two = 0, len(arr) - 1
+    next_zero, next_two = 0, len(nums) - 1
     i = 0
     # imp condition for termination
     while i <= next_two:
-        if arr[i] == 0:
+        if nums[i] == 0:
             # swap with element at next_zero index
-            arr[next_zero], arr[i] = arr[i], arr[next_zero]
+            nums[next_zero], nums[i] = nums[i], nums[next_zero]
             # increment next_zero
             next_zero += 1
             # the element at next_zero has to be 1 ?
@@ -17,9 +17,9 @@ def dutch_flag_sort(arr):
             # so element at next_zero has to be 1
             # hence we can increment i also
             i += 1
-        elif arr[i] == 2:
+        elif nums[i] == 2:
             # swap with element at next_two index
-            arr[next_two], arr[i] = arr[i], arr[next_two]
+            nums[next_two], nums[i] = nums[i], nums[next_two]
             # decrement next_two
             next_two -= 1
             # since this element could be 0 or 2 we cannot move to next element
@@ -28,7 +28,7 @@ def dutch_flag_sort(arr):
             # arr[i] == 1
             i += 1
 
-    return arr
+    return nums
 
 
 def main():
@@ -36,9 +36,9 @@ def main():
     # dutch_flag_sort(arr)
     # print(arr)
 
-    arr = [2, 2, 0, 1, 2, 0]
-    dutch_flag_sort(arr)
-    print(arr)
+    nums = [2, 2, 0, 1, 2, 0]
+    dutch_flag_sort(nums)
+    print(nums)
 
 
 main()
